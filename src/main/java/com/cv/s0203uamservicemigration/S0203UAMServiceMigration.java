8df8e290bfcc5d13bb73a7ca1c7f3a46dc356437
ramakrishna <ram_r@live.com>
2025-03-22 19:05:42 +0400

    BASE

---
+++ src/main/java/com/cv/s0203uamservicemigration/S0203UAMServiceMigration.java
@@ -0,0 +1,13 @@
+package com.cv.s0203uamservicemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S0203UAMServiceMigration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S0203UAMServiceMigration.class, args);
+    }
+
+}
---
+++ src/test/java/com/cv/s0203uamservicemigration/S0203UAMServiceMigrationTests.java
@@ -0,0 +1,13 @@
+package com.cv.s0203uamservicemigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S0203UAMServiceMigrationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
